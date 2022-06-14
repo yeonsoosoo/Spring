616ca5367fdb1ec2efc45bc4e0576394ccdfd256
Park Yeonsoo <lovepys11@naver.com>
2022-06-14 16:51:20 +0900

    [Spring] 기본구조 실습

--- Ex_0614_SpringDI/src/main/java/com/korea/testdi/BoardController.java
+++
@@ -1,5 +0,0 @@
-package com.korea.testdi;
-
-public class BoardController {
-
-}
--- Ex_0614_SpringDI/src/main/java/service/BoardService.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-import java.util.List;
-
-public interface BoardService {
-	List<Object> selectList();
-}
